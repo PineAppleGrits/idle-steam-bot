59bfcbc48e4ec6546120f4b5b060331712f5c507
Gino S <66481194+PineAppleGrits@users.noreply.github.com>
2021-06-08 10:12:16 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 /*
     Project: Steam Hour Bot
-    Created by Swayer
     version.1.3.9.
 */
 
